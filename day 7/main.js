32237664748d3009b38737900b7b89412eb351c5
Rahul rai <rrai06125@gmail.com>
2022-07-14 23:42:17 +0530

    Initial commit

---
+++ day 7/main.js
@@ -0,0 +1,12 @@
+// const form = document.getElementById("form")
+
+// function onSubmit(e){
+//     e.preventDefault()
+//     const first_name = e.target.first_name.value;
+//     const last_name = e.target.last_name.value;
+//     const email = e.target.email.value;
+// }
+
+// form.addEventListener("submit",onSubmit)
+
+
