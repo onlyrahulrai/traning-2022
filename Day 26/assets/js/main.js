cd53b0050af0655fdbf8858799661b593c0f0088
Rahul rai <rrai06125@gmail.com>
2022-08-22 23:47:06 +0530

    day-26 :) Javascript basic

---
+++ Day 26/assets/js/main.js
@@ -0,0 +1,218 @@
+/* 
+    Datatypes
+        1.Primitive 
+        2.non-primitive Datatypes
+*/
+
+
+/*
+
+Primitive Datatypes
+1.Number
+2.BigInt
+3.String
+4.null 
+5.undefined
+6.Symbol
+7.Boolean
+
+Non-primitive Datatypes
+8.Objects or Array
+
+*/
+
+// var num = 123;
+
+// var num = 45;
+ 
+// var num = 60;
+
+// console.log(num);
+
+
+// function func(){
+//     console.log(num)
+// }
+
+// func();
+
+// {
+//     console.log(num)
+// }
+
+
+/*
+// Block Scope
+    1.let 
+    2.const 
+*/
+
+/*
+    Global Scope
+    3.var
+*/
+
+// let num = 123; // declartions
+
+// num = 45; // Initialization
+
+// num = 90;
+
+// {
+//     let num = 123;
+//     console.log(num);
+// }
+
+// num = 34;
+
+
+// console.log("Normally print",num)
+
+// function func(){
+//     console.log("print from function ",num);
+// }
+
+// func()
+
+// {
+//     console.log("show in block",num)
+// }
+
+
+// Hosting
+// console.log(name);
+// var name = 123;
+
+// var name = Infinity;
+// console.log(name)
+
+/* 
+    123
+    12.7
+    Inifinity
+    MAX_VALUE
+    MIN_VALUE
+    NAN
+*/
+
+// alert('a'/3);
+
+// NaN - Not a number
+
+/*
+
+// 1.Number 
+const numInt = 123;
+const numFloat = 123.45;
+const numInfinity = Infinity;
+console.log(typeof numInt)
+console.log(typeof numFloat)
+console.log(typeof numInfinity)
+
+*/
+
+/* 
+
+// 2. BigInt
+const bigint = 123456789012345678901234567890n;
+
+console.log(typeof(bigint))
+
+*/
+
+// 3.String
+/* 
+    ''
+    ""
+    ``
+// const str1 = "Javascript";
+// const str2 = 'Javascript';
+// const str3 = `Javascript`;
+// console.log(typeof(str1))
+// console.log(typeof(str2))
+// console.log(typeof(str3))
+
+*/
+
+// 4.Boolean
+// const boolTrue = true;
+// const boolFalse = true;
+// console.log(typeof(boolTrue))
+// console.log(typeof(boolFalse))
+
+// 5.null
+
+// const Null = null;
+// console.log(typeof Null);
+
+// 6.undefined
+// let name;
+// let age = undefined;
+// console.log(name);
+// console.log(age);
+
+
+// 7.Symbol
+// const symbol = Symbol('1')
+// console.log(typeof symbol)
+
+// const num1 = '5';
+// const num2 = 5;
+// const num3 = num1 + num2;
+// console.log(typeof num3);
+
+/*
+const num1 = +'m';
+const num2 = Number('9');
+
+console.log(typeof num1);
+console.log(typeof num2);
+*/
+
+// let name = 'llya';
+
+// alert(`hello ${1}`);
+// alert(`hello ${'name'}`);
+// alert(`hello ${name}`);
+
+/*
+console.log('hello ' + 1) // - hello 1
+console.log('hello' + 'name') // - hello name
+console.log('hello' + name) // - hello llya
+*/
+
+// Interaction: alert,prompt,confirm
+// alert("Hello world!")
+
+// const age = prompt("How old are you: ")
+
+// const isConfirmed = confirm(`Are you sure? you are ${age} year old`);
+
+// console.log(isConfirmed)
+
+// if(isConfirmed){
+//     console.log(`yes, I'm ${age} year old`)
+// }else{
+//     console.log(`I'm Not ${age} year old`)
+// }
+
+
+let name = prompt("What is you name? ")
+alert(name)
+
+
+/* 
+    1.What is output of these expreration.
+        +true 
+        +false
+        +0
+        +''
+        3 + 'a'
+        3 - 'a'
+        3 * 'a' 
+        3 / 'a'
+        'a' / 3
+*/
+
+
+
